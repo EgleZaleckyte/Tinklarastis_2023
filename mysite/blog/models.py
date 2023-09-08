023a27e3304e93b59dd8676beecc24260dd80d1c
Egle Zaleckyte <e.zaleckytes@gmail.com>
2023-09-08 14:55:45 +0300

    Sukurti projekto modeliai Post ir Comment , modeliai užregistuoti admin svetaineje (admin.py).

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post (models.Model):
+    title = models.CharField(verbose_name="Pavadinimas",max_length=100)
+    content = models.TextField(verbose_name="Tekstas",max_length=8000)
+    created = models.DateTimeField(verbose_name="Sukūrimo data", auto_now_add=True)
+    user = models.ForeignKey(User, verbose_name="Autorius",on_delete=models.SET_NULL,null=True)
+
+class Comment (models.Model):
+    content = models.TextField(verbose_name="Tekstas", max_length=1000)
+    created = models.DateTimeField(verbose_name="Sukūrimo data", auto_now_add=True)
+    post = models.ForeignKey(to = "Post",verbose_name="Įrašas", on_delete=models.CASCADE,related_name="comments")
+    user = models.ForeignKey(User, verbose_name="Autorius",on_delete=models.SET_NULL,null=True)
+
+
